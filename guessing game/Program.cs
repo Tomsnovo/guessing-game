f86d5e983377784d853cd450acd432699d6c6ad3
Tomáš Bejček <Bejcek.Tomas@stud.vassboskovice.cz>
2021-11-16 09:24:53 +0100

    guessing game

--- guessing game/Program.cs
+++ guessing game/Program.cs
@@ -6,7 +6,37 @@ namespace guessing_game
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random random = new Random();
+            int randomNum;
+            int guessedNum;
+            bool wrongAns = true;
+            randomNum = random.Next(1, 101);
+
+            Console.WriteLine("Guessing game");
+            Console.WriteLine("______________");
+            Console.WriteLine("I think about number from 1 to 100");
+            
+
+
+            while (wrongAns)
+            {
+                Console.WriteLine("Guess random number:");
+                guessedNum = int.Parse(Console.ReadLine());
+                if (guessedNum > randomNum)
+                {
+                    Console.WriteLine("Too high");
+                }
+                else if (guessedNum < randomNum)
+                {
+                    Console.WriteLine("Too low");
+                }
+                else
+                {
+                    Console.WriteLine("You´re right");
+                    wrongAns = false;
+                }
+                
+            }
         }
     }
 }
